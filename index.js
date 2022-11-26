52ebd389b9c076ebab4092ee71759afe87452c1b
Akash Vaghela <akashvaghela09@gmail.com>
2022-11-27 00:47:08 +0530

    rollback changes

--- index.js
+++ index.js
@@ -1,65 +1,65 @@
-// const express = require('express');
-// const app = express();
-// const PORT = 3000;
-// const { style } = require("./style");
+const express = require('express');
+const app = express();
+const PORT = 3000;
+const { style } = require("./style");
 
 
-// app.get('/', (req, res) => {
-//   res.send(`<div style="${style}">It's a Sample Node App</div>`)
-// })
+app.get('/', (req, res) => {
+  res.send(`<div style="${style}">It's a Sample Node App</div>`)
+})
 
-// app.get('/app', (req, res) => {
-//     res.send(`GET Request`)
-// })
+app.get('/app', (req, res) => {
+    res.send(`GET Request`)
+})
 
-// app.post('/app', (req, res) => {
-//     res.send(`POST Request`)
-// })
+app.post('/app', (req, res) => {
+    res.send(`POST Request`)
+})
 
-// app.patch('/app', (req, res) => {
-//     res.send(`PATCH Request`)
-// })
+app.patch('/app', (req, res) => {
+    res.send(`PATCH Request`)
+})
 
-// app.put('/app', (req, res) => {
-//     res.send(`PUT Request`)
-// })
+app.put('/app', (req, res) => {
+    res.send(`PUT Request`)
+})
 
-// app.delete('/app', (req, res) => {
-//     res.send(`DELETE Request`)
-// })
-
-
-
-
-// const testing = () => {
-//     console.log("working.....");
-// }
-
-// testing()
+app.delete('/app', (req, res) => {
+    res.send(`DELETE Request`)
+})
 
 
 
 
+const testing = () => {
+    console.log("working.....");
+}
 
-// app.listen(process.env.PORT || PORT, () => {
-//     console.log(`Node running at ${PORT}`)
-// })
+testing()
 
-const SMTPServer = require("smtp-server").SMTPServer;
-const parser = require("mailparser").simpleParser
 
-const server = new SMTPServer({
-  onData(stream, session, callback) {
-    parser(stream, {}, (err, parsed) => {
-      if (err)
-        console.log("Error:" , err)
+
+
+
+app.listen(process.env.PORT || PORT, () => {
+    console.log(`Node running at ${PORT}`)
+})
+
+// const SMTPServer = require("smtp-server").SMTPServer;
+// const parser = require("mailparser").simpleParser
+
+// const server = new SMTPServer({
+//   onData(stream, session, callback) {
+//     parser(stream, {}, (err, parsed) => {
+//       if (err)
+//         console.log("Error:" , err)
       
-      console.log(parsed)
-      stream.on("end", callback)
-    })
+//       console.log(parsed)
+//       stream.on("end", callback)
+//     })
     
-  },
-  disabledCommands: ['AUTH']
-});
+//   },
+//   disabledCommands: ['AUTH']
+// });
 
-server.listen(25, "35.160.120.126")
+// server.listen(25, "35.160.120.126")
